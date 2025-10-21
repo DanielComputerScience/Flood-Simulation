07eb97d85ef6ab8f2c3f72fd62c73749fb51dc2b
elijahwilliams <elijahwilliams@penguin>
2025-10-21 10:59:35 -0500

    trying to add second array room

--- home.js
+++ home.js
@@ -21,6 +21,7 @@ floodButton.addEventListener('click', startFlood);
 const water = document.querySelector('.water');
 const grass = document.querySelector('.grass');
 const drain = document.querySelector('.drain');
+const sroom = document.querySelector('.sroom');
 grass.style.height = grassW + 'px';
 grass.style.height = grassH + 'px';
 drain.style.backgroundColor = "black";
@@ -41,6 +42,28 @@ function pipeFlow() {
 
 	}
 }
+function generateMap() {
+	const roomElement = document.getElementById('sroom');
+	const gridElement = document.createElement('div');
+	var gridH = 0;
+	var gridW = 0;
+	var width = sroom.style.width;
+	var height = sroom.style.height;
+	var rows = height / 10;
+	var columns = width / 40;
+	for (let i = 0; i < rows; i++) {
+		
+		for (let j = 0; j < columns; j++) {
+			gridElement.classList.add('grid');
+			gridElement.style.bottom = gridH + 'px'; 
+			gridElement.style.left = '${gridW}px';
+			roomElement.appendChild(gridElement);
+			gridW += 40
+		}
+		gridH += 10
+	}
+}
+generateMap()
 var grassArea = grassH * grassW;
 setInterval(() => {
 	if (flood == true) {
